6ea10f65f77f2cfbbd41ebf66848362e780571c0
Ezequielagradnik <ezequielagradnik@gmail.com>
2024-11-24 19:40:00 -0300

    empece con el front

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-4xl font-bold text-blue-500">¡Marketplace Funciona!</h1>
+    </div>
+  );
+};
+
+export default App;
+
+
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+const tailwindConfig = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default tailwindConfig;
